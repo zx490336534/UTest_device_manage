5980cbe99cfb84a83fe5ba9c1ed2f30115c8d1e3
zhongxin <490336534@qq.com>
2019-12-07 00:19:55 +0800

    添加接口文档docs

--- UTest_device_manage/urls.py
+++ UTest_device_manage/urls.py
@@ -36,5 +36,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('device_manage.urls')),
+    path('docs/', include_docs_urls(title='自动化设备管理平台',
+                                    description='自动化测试设备管理')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

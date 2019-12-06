3f3c72bbbbdb72efe4b8f7908e53c4a4235648ab
zhongxin <490336534@qq.com>
2019-12-06 19:46:50 +0800

    初始化项目

--- UTest_device_manage/wsgi.py
+++ UTest_device_manage/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for UTest_device_manage1 project.
+WSGI config for UTest_device_manage project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UTest_device_manage1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UTest_device_manage.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UTest_device_manage1.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UTest_device_manage.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

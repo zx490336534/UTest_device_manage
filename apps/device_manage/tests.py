544567034e956165f1c54069be440165e7885429
zhongxin <490336534@qq.com>
2019-12-06 22:17:08 +0800

    空闲设备查找，逻辑待完善

--- apps/device_manage/tests.py
+++ apps/device_manage/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+import requests
 
-# Create your tests here.
+base_url = 'http://127.0.0.1:8000/device_manage/'
+data = {'type': 'tool'}
+response = requests.post(f'{base_url}status/', json=data)
+print(response)
--- apps/device_manage/views.py
+++ apps/device_manage/views.py
@@ -7,6 +7,8 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 
+from device_manage.utils import get_count_by_device_info
+from utils.timeoperator import change_time
 from . import serializers
 from .models import Device_info
 
@@ -34,8 +36,48 @@ class DeviceViewSet(ModelViewSet):
     """
     queryset = Device_info.objects.filter(is_delete=False)
     serializer_class = serializers.DeviceModelSerializer
-    ordering_fields = ('id', 'name')
+    ordering_fields = ('id', 'device_ip')
 
     def perform_destroy(self, instance):
         instance.is_delete = True
         instance.save()  # 逻辑删除
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        response.data['results'] = get_count_by_device_info(response.data['results'])
+        return response
+
+    @action(methods=['post'], detail=False)
+    def status(self, request):
+        """
+        获取设备状态
+        """
+        data = request.data
+        device_ip = data.get('device_ip')
+        device_name = data.get('device_name')
+        type = data.get('type')
+        device_info_objs = self.queryset
+        if device_ip:
+            # 根据设备IP查找,返回一个
+            device_info_objs = Device_info.objects.filter(is_delete=False, device_ip=device_ip).first()
+        elif device_name:
+            # 根据设备名称查找,返回全部
+            device_info_objs = Device_info.objects.filter(is_delete=False, device_name=device_name).all()
+        elif type:
+            if type == 'daily':
+                # 每日编译状态为0 且设备使用状态为0,返回一个
+                device_info_objs = Device_info.objects.filter(is_delete=False, daily_status=0, use_status=0).first()
+            elif type == 'tool':
+                # 有工装IP 同一工装电脑下设备使用状态为0,返回全部
+                device_info_objs = Device_info.objects.filter(is_delete=False,
+                                                              tool_ip__isnull=False,
+                                                              use_status=0).all()
+            elif type == 'unuse':
+                # 任意非使用状态的设备,返回全部
+                device_info_objs = Device_info.objects.filter(is_delete=False, use_status=0).all()
+        else:
+            return Response({'error': '请使用正确的查询方式'})
+        serializer = self.get_serializer(device_info_objs, many=False)
+        new_data = serializer.data
+        new_data['create_time'] = change_time(new_data['create_time'])
+        return Response(new_data)
